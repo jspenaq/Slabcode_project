7bd5a70b7888ee08697df8f6c9878a1f75c10a7a
Sebastian Peña <jspenaq@unal.edu.co>
2021-01-27 23:32:47 -0500

    :hammer: deleted unnecessary comments

--- slabcode/urls.py
+++ slabcode/urls.py
@@ -28,13 +28,3 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
-# router = routers.DefaultRouter()
-# router.register(r'project', views.apiOverview)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     path('api/', include('rest_framework.urls', namespace='rest_framework'))
-# ]
